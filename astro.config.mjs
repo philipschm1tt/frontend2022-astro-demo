3376aa34ffb92174ae36ecf1988017bbe0d8c46f
Philip Schmitt <philip.schmitt@bestsecret.com>
2022-06-11 12:09:58 +0200

    Install Netlify adapter for Astro SSR
    Resources:
    - https://astro.build/blog/experimental-server-side-rendering/
    - https://www.netlify.com/blog/astro-ssr/
    - https://github.com/withastro/astro/tree/main/packages/integrations/netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  adapter: netlify()
 });
