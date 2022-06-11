5e06149bd5ce4c35bba48bdf3e28da815baffb81
Philip Schmitt <philip.schmitt@bestsecret.com>
2022-06-11 13:27:12 +0200

    Add Vue components using Vant
    Resources:
    - https://github.com/youzan/vant
    - https://youzan.github.io/vant/#/en-US/quickstart
    - https://youzan.github.io/vant/#/en-US/tab
    - https://destiner.io/blog/post/using-vue-components-inside-astro/
    - https://vuejsdevelopers.com/2021/10/26/vue-astro/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
-
 import react from "@astrojs/react";
+import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  adapter: netlify()
-});
+  integrations: [react(), tailwind(), vue()],
+  adapter: netlify(),
+  'vite.ssr.noExternal': ['vant'],
+})
