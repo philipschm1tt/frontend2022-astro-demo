793db0e4366b4cf4b5ee8c62a9708b2c7617fcf7
Philip Schmitt <philip.schmitt@bestsecret.com>
2022-06-12 17:39:39 +0200

    Replace Svelte carousel
    Carousel did not work on Netlify.

--- src/components/CarouselSvelte.svelte
+++ src/components/CarouselSvelte.svelte
@@ -1,15 +1,18 @@
 <script>
-  import Carousel from '@beyonk/svelte-carousel'
+  import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+  import '@splidejs/svelte-splide/css';
 
   let items = [1,2,3,4,5]
 </script>
 
 <template>
-  <Carousel perPage=1 autoplay=5000>
+  <Splide>
     {#each items as index}
-      <div class="h-40 bg-blue-400 relative">
-        <h3 class="my-0 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{index}</h3>
-      </div>
+      <SplideSlide>
+        <div class="h-40 bg-blue-400 relative">
+          <h3 class="my-0 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{index}</h3>
+        </div>
+      </SplideSlide>
     {/each}
-  </Carousel>
+  </Splide>
 </template>
