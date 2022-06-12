0500ceb47d3cc1f41e0a3b6eebdd10e5586b0400
Philip Schmitt <philip.schmitt@bestsecret.com>
2022-06-12 17:09:36 +0200

    Hydrate carousel when visible
    Problem: React carousel produces error on Netlify.
    
    Problem: Svelte does not get hydrated.

--- src/components/CarouselReact.tsx
+++ src/components/CarouselReact.tsx
@@ -5,7 +5,7 @@ export default function CarouselReact() {
 	return (
 		<AwesomeSlider>
 			{
-				[1, 2, 3, 4, 5].map((number, index) =>
+				['1', '2', '3', '4', '5'].map((number, index) =>
 					<div key={index} className="h-40 bg-blue-400 relative">
 						<h3 className="my-0 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{number}</h3>
 					</div>)
